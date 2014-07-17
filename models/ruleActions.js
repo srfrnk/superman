2f0030a759ae6452f3329a2b8df9ca20a17f328d
srfrnk <srfrnk@gmail.com>
2014-07-17 19:44:06 +0300

    added proxy

--- models/ruleActions.js
+++ models/ruleActions.js
@@ -5,13 +5,19 @@ require("requirejs").define("models/ruleActions", ["models/mongooseHelper"], fun
         "static-file": function (rule, req, res,proxyInfo) {
             var filePath = path.join(req.application.get("__dirname"), rule.filePath);
             res.sendfile(filePath);
-            return "Static-File";
+            return "Static-File => "+rule.filePath;
         },
         "pass-through": function (rule, req, res,proxyInfo) {
             proxyInfo.proxy.web(req, res, {target: req.url}, function (err) {
                 console.log("proxy pass through error:",err);
             });
             return "Pass-Through";
-        }
+        },
+        "proxy": function (rule, req, res,proxyInfo) {
+            proxyInfo.proxy.web(req, res, {target: req.target}, function (err) {
+                console.log("proxy error:",err);
+            });
+            return "Proxy => "+rule.target;
+        }        
     };
 });
